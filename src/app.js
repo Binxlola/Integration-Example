9a5928f887f9f1a1e915d562d404f2f61b0cc355
Jason Smit <jason.michael.smit@gmail.com>
2021-03-13 19:09:09 +1300

    Linter setup

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
-const dayOfTheWeek = (date = new Date()) => {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-    return days[date.getDay()];
+function dayOfTheWeek(date = new Date()) {
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  return days[date.getDay()];
 }
 
+const monthOfTheYear = (date = new Date()) => {
+  const months = ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  return months[date.getMonth()];
+};
 
 try {
-    document.getElementById('day').innerText = dayOfTheWeek();
-} catch(err) {}
-
+  document.getElementById('day').innerText = dayOfTheWeek();
+} catch (err) {
+  console.log('There was an error');
+}
 
 exports.dayOfTheWeek = dayOfTheWeek;
+exports.monthOfTheYear = monthOfTheYear;
--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,11 @@
-const { dayOfTheWeek } = require('./app');
-
+import { dayOfTheWeek, monthOfTheYear } from './app';
 
 test('getDay returns the long-format day of the week', () => {
-    const day = dayOfTheWeek( new Date('3/11/2020') );
-    expect( day ).toBe('Wednesday');
+  const day = dayOfTheWeek(new Date('3/11/2020'));
+  expect(day).toBe('Wednesday');
+});
+
+test('getYear returns the long-format month of the year', () => {
+  const month = monthOfTheYear(new Date('3/11/2020'));
+  expect(month).toBe('March');
 });
